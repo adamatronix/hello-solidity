42a735d25433b0000e708dbf5a9b28b334a96779
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-03-15 17:02:08 +0800

    master Install web3 for working with the blockchain and extract the contract object

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const Web3 = require('web3');
 const fs = require('fs');
 const solc = require('solc');
 
+const provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545");
+const web3 = new Web3(provider);
 const content = fs.readFileSync('HelloWorld.sol', 'utf-8');
 
 const input = {
@@ -20,4 +23,10 @@ const input = {
 }
 
 const output = JSON.parse(solc.compile(JSON.stringify(input)));
-console.log(output);
+
+const { HelloWorld } = output.contracts['HelloWorld.sol'];
+const { abi, evm } = HelloWorld;
+
+const contract = new web3.eth.Contract(abi);
+
+console.log(contract);
